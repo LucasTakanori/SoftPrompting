ff722af79308a0dfb92c9855c302cc34adf090aa
marccasals98 <marc.casals8@gmail.com>
2024-05-30 11:41:26 +0200

    more things implemented

--- src/model.py
+++ src/model.py
@@ -4,7 +4,7 @@ import torch
 import whisper
 from whisper.tokenizer import get_tokenizer
 from soft_prompts import SoftPrompting
-
+from asr import Whisper
 
 # Set logging config
 
@@ -30,22 +30,14 @@ class PromptASR(nn.Module):
         self.device = device
         self.parameters = parameters
     
-    def select_whisper(self):
-        self.model = whisper.load_model(self.parameters.whisper_flavour, self.parameters.device)
-        self.tokenizer = get_tokenizer(self.parameters.whisper_flavour)
-    
     def init_asr(self):
-        self.select_whisper()
-        for name, parameter in self.model.named_parameters():
-            logger.info(f"Parameter {name} requires grad: False")
-            parameter.requires_grad = False
-        # self.model.eval()
-    
+        if self.parameters.asr_model == 'whisper':
+            self.asr = Whisper(self.parameters)
+        
     def init_soft_prompting(self):
         self.soft_prompting = SoftPrompting(self.parameters)
 
     def forward(self, input_tensor) -> torch.Tensor:
-        
-        logits = self.
+        logits = self.asr(input_tensor, self.soft_prompting())   
         return logits
     
